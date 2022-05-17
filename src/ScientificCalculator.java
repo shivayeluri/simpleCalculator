35a4b6b234864222953ed85687034f89cb5d55af
shivakumar.y <shivakumar.y@perfios.com>
2022-05-17 16:45:26 +0530

    Added : To implement scintific calulator

---
+++ src/ScientificCalculator.java
@@ -0,0 +1,9 @@
+public class ScientificCalculator {
+    public static void main(String[] args) {
+        int num = 1000;
+        int mul = 2;
+        int value = num*mul;
+        System.out.println("value : "+value+1);
+        System.out.println("newvalue : "+mul*mul);
+    }
+}
